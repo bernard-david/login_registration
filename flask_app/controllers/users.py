d0cb9eb9f45f4a9af6f09bd1ceab08b7fa0dd33a
bernard-david <dpbernard18@gmail.com>
2021-06-21 16:53:41 -0400

    user is stored and session. user can also logout properly.

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -6,13 +6,14 @@ bcrypt = Bcrypt(app)
 
 @app.route("/")
 def login_register():
+    # renders the main page with registration form and login
     return render_template("login_register.html")
 
-@app.route("/user/create", methods=["POST"])
+@app.route("/create", methods=["POST"])
 def create_user():
+    # validate the form
     if User.validate(request.form):
-        print("#"*80)
-        print("Success!")
+        # if valid then hash the password and save it to a dictionary
         pw_hash = bcrypt.generate_password_hash(request.form['password'])
         data = {
             "first_name": request.form['first_name'],
@@ -20,24 +21,24 @@ def create_user():
             "email": request.form["email"],
             "password": pw_hash
         }
-        user_id = User.create(data)
-        session["user_id"] = user_id
-        return redirect(f"/user/{user_id}")
+        # create the new user
+        User.create(data)
+        # save that user into session
+        session["first_name"] = data["first_name"]
+        flash("You've been logged in!")
+        return redirect("/success")
     else:
-        print("#"*80)
-        print("Failed!")
+        # if form is invalid, redirect to the main page with the registration form
         return redirect("/")
 
-@app.route("/user/<int:user_id>")
-def welcome_user(user_id):
-    data = {
-        'id': user_id
-    }
-    user = User.get_user(data)
+@app.route("/success")
+def welcome_user():
+    if not "first_name" in session:
+        flash("You need to login first!")
+        return redirect("/")
+    return render_template('welcome_user.html')
 
-    return render_template('welcome_user.html', user = user)
-
-@app.route("/user/login", methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     data = { "email": request.form["email"]}
     user_in_db = User.get_by_email(data)
@@ -49,6 +50,13 @@ def login():
         flash("Invalid Email/Password")
         return redirect("/")
     
-    session['user_id'] = user_in_db.id
-    user_id = session['user_id']
-    return redirect(f"/user/{user_id}")
+    session['first_name'] = user_in_db.first_name
+    flash("You've been logged in!")
+    return redirect("/success")
+
+@app.route("/logout")
+def logout():
+    if "first_name" in session:
+        flash("You've been logged out.")
+        session.pop("first_name", None)
+        return redirect("/")
